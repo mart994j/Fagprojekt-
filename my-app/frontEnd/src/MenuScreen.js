e3038079d62fa3b3f5230e97579b39cfef7e17c7
Martin Jespersen <martinjespersen9@gmail.com>
2024-02-09 14:15:49 +0100

    velkomst teks

--- my-app/frontEnd/src/MenuScreen.js
+++ my-app/frontEnd/src/MenuScreen.js
@@ -9,7 +9,8 @@ function MenuScreen() {
   };
 
   return (
-    <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
+    <div style={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
+      <h1 style={{ marginBottom: '20px', fontSize: '60px' }}>Velkommen til Sudoku!</h1>
       <button onClick={handleStartGame} style={{ padding: '10px 20px', fontSize: '20px', cursor: 'pointer' }}>
         Start Spil
       </button>
