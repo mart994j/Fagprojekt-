d17f1d1a00d3dedc45267fc7f82e04cf9bbe7a23
Sebastian Wulf-Andersen <sebastian@wulfandersen.dk>
2024-02-21 15:58:25 +0100

    pause virker

---
+++ my-app/frontEnd/src/Components/sudokuPause.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './CSS/pauseModal.css'
+
+const sudokuPause = ({ isPaused, onContinue }) => {
+  if (!isPaused) return null;
+
+  return (
+    <div className="pauseModalOverlay">
+      <div className="pauseModal">
+        <h2>Paused</h2>
+        <button onClick={onContinue} style={{marginLeft: '10px'}}>Continue</button>
+      </div>
+    </div>
+  );
+};
+
+export default sudokuPause;
