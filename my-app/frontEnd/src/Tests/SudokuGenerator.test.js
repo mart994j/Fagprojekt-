ab105a532ed8f92261a590b7e443b066c767eb7c
Martin Jespersen <martinjespersen9@gmail.com>
2024-06-06 11:37:08 +0200

    sudokugen test

--- my-app/frontEnd/src/Tests/SudokuGenerator.test.js
+++ my-app/frontEnd/src/Tests/SudokuGenerator.test.js
@@ -1,6 +1,6 @@
 //npm install --save-dev jest
 
-const { SudokuGenerator } = require('./SudokuGenerator');
+const { SudokuGenerator } = require('../../../backEnd/SudokuGenerator');
 
 describe('SudokuGenerator', () => {
 
